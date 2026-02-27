f994a8d30fd574e3b15f64cf42c1a8ef2caaf13c
Ahmedhosamghorab <ahmedhossamfarag@gmail.com>
2026-02-27 14:12:07 +0200

    feat(common): add shared utility functions

---
+++ src/utils/interceptors/logger.Interceptors.ts
@@ -0,0 +1,17 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(tap(() => console.log(`after interceptor`)));
+  }
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+export type JWTPayload = {
+  id: number;
+  userType: string;
+};
+export type AccessToken = {
+  accessToken: string;
+};
